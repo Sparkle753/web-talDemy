0a859ef54a9672711eecf81395b03645b0170af1
SparklePh <park_1112@windowslive.com>
2022-07-19 00:37:06 +0700

    add: App

---
+++ my-app/src/components/HomeDescription.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class = "homeDescription">
+        <h1>Prophet: description</h1>
+        <h1>Guidance: description</h1>
+        <h1>Academic: description</h1>
+        <h1>Trust: description</h1>
+        <h1>Kaching: description</h1>
+    </div>    
+</template>
+
+<script>
+export default {
+  name: 'HomeDescription',
+  data(){
+      return {} 
+    }
+
+}
+</script>
+
+<style>
+    .homeDescription > h1{
+        border: 1px solid black;
+        text-align: left;
+        padding-bottom: 30%;       
+    }
+
+</style>
---
+++ my-app/src/components/HomeMenu.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class = "homeManu">
+      <img :src = logo :width=" sizeImage" :height="sizeImage">
+      <h1> Prophet </h1>
+      <h1> Guidance </h1>
+      <h1> Academic </h1>
+      <h1> Trust</h1>
+      <h1> Kaching </h1>
+      <h1> Login/Signup as Recruiter </h1>
+      <h1> Login/Signup as Student </h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeManu',
+  data(){
+      return {
+        logo: require('@/assets/logo/Wordpress Transparent.png'),
+        sizeImage : 50
+      } 
+    }
+
+}
+</script>
+
+<style>
+  .homeManu{
+    display: flex;
+  }
+
+  .homeManu > h1{
+    margin:  0 auto;
+    border: 1px solid black;
+    text-align: center;
+    flex: 100%;
+  }
+
+  
+
+</style>
